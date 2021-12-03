db7109a1aad4022a9972ceed514109e95164c5c1
ced <cedric.pk22@gmail.com>
2021-12-03 10:01:35 -0500

    update truffle config [Truffle]

--- truffle-config.js
+++ truffle-config.js
@@ -14,14 +14,14 @@ module.exports = {
     testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
-      confirmations: 10,
+      confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
     },
     bsc: {
       provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
-      confirmations: 10,
+      confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
     },
